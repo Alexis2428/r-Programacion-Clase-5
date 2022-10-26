68a36fec97d5c2cf034d555a065cb9af961b4045
Alexis Landa <114886294+Alexis2428@users.noreply.github.com>
2022-10-26 16:08:20 -0300

    Agrega funcionalidad a la pagina
    Se agrega la funcionalidad para hacer lo que pide la tarea, en base a una lista de prueba

--- tareas/tarea4.js
+++ tareas/tarea4.js
@@ -5,3 +5,52 @@
 // 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
 // 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
 
+function calcularPromedio(numeros) {
+    let sumaTotal = 0
+    for (let i = 0; i < numeros.length; i++) {
+        sumaTotal += Number(numeros[i].textContent);
+    }
+    return sumaTotal / numeros.length;
+}
+function obtenerNumeroPequeño(numeros) {
+    let numeroPequeño = numeros[0].textContent;
+    for (let i = 1; i < numeros.length; i++) {
+        if (numeroPequeño > numeros[i].textContent){
+            numeroPequeño = numeros[i].textContent;
+        }
+    }
+    return numeroPequeño;
+}
+function obtenerNumeroGrande(numeros) {
+    let numeroGrande = numeros[0].textContent;
+    for (let i = 1; i < numeros.length; i++) {
+        if (numeroGrande < numeros[i].textContent){
+            numeroGrande = numeros[i].textContent;
+        }
+    }
+    return numeroGrande;
+}
+function obtenerNumeroMasRepetido(numeros) {
+    let numeroMasRepetido;
+    let cantidadRepetido = 0;
+    for (let i = 0; i < numeros.length; i++){
+        let repetido = 0;
+        for (let j = i + 1; j < numeros.length; j++){
+            if (numeros[i].textContent === numeros[j].textContent) {
+                repetido++;
+            }
+        }
+        if (cantidadRepetido < repetido) {
+            numeroMasRepetido = numeros[i].textContent;
+            cantidadRepetido = repetido;
+        }
+
+    }
+    return numeroMasRepetido;
+}
+
+let numeros = document.querySelectorAll('.lista');
+document.querySelector('#promedio').textContent = calcularPromedio(numeros);
+document.querySelector('#numero-pequeño').textContent = obtenerNumeroPequeño(numeros);
+document.querySelector('#numero-grande').textContent = obtenerNumeroGrande(numeros);
+document.querySelector('#numero-frecuente').textContent = obtenerNumeroMasRepetido(numeros);
