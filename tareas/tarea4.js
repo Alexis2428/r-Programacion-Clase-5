bcefe3adca27dc61e59defd2e48279771eee56c3
Alexis Landa <114886294+Alexis2428@users.noreply.github.com>
2022-10-26 20:06:38 -0300

    Mejora funcionalidad de la pagina
    Se mejora la funcionalidad para que sea el usuario el que ingrese los datos de la lista a usar

--- tareas/tarea4.js
+++ tareas/tarea4.js
@@ -5,27 +5,39 @@
 // 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
 // 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
 
+function crearRespuesta(texto, id){
+    const $elementoP = document.querySelector('#texto-resultado');
+    const nodoTexto = document.createTextNode(texto);
+    $elementoP.appendChild(nodoTexto);
+    const elementoEm = document.createElement('em');
+    elementoEm.setAttribute('id', id);
+    $elementoP.appendChild(elementoEm);
+    const siguienteLinea = document.createElement('br');
+    $elementoP.appendChild(siguienteLinea);
+    return false
+}
+
 function calcularPromedio(numeros) {
     let sumaTotal = 0
     for (let i = 0; i < numeros.length; i++) {
-        sumaTotal += Number(numeros[i].textContent);
+        sumaTotal += Number(numeros[i].value);
     }
     return sumaTotal / numeros.length;
 }
-function obtenerNumeroPequeño(numeros) {
-    let numeroPequeño = numeros[0].textContent;
+function obtenerNumeroPequenio(numeros) {
+    let numeroPequenio = numeros[0].value;
     for (let i = 1; i < numeros.length; i++) {
-        if (numeroPequeño > numeros[i].textContent){
-            numeroPequeño = numeros[i].textContent;
+        if (numeroPequenio > numeros[i].value) {
+            numeroPequenio = numeros[i].value;
         }
     }
-    return numeroPequeño;
+    return numeroPequenio;
 }
 function obtenerNumeroGrande(numeros) {
-    let numeroGrande = numeros[0].textContent;
+    let numeroGrande = numeros[0].value;
     for (let i = 1; i < numeros.length; i++) {
-        if (numeroGrande < numeros[i].textContent){
-            numeroGrande = numeros[i].textContent;
+        if (numeroGrande < numeros[i].value) {
+            numeroGrande = numeros[i].value;
         }
     }
     return numeroGrande;
@@ -33,15 +45,15 @@ function obtenerNumeroGrande(numeros) {
 function obtenerNumeroMasRepetido(numeros) {
     let numeroMasRepetido;
     let cantidadRepetido = 0;
-    for (let i = 0; i < numeros.length; i++){
+    for (let i = 0; i < numeros.length; i++) {
         let repetido = 0;
-        for (let j = i + 1; j < numeros.length; j++){
-            if (numeros[i].textContent === numeros[j].textContent) {
+        for (let j = i + 1; j < numeros.length; j++) {
+            if (numeros[i].value === numeros[j].value) {
                 repetido++;
             }
         }
         if (cantidadRepetido < repetido) {
-            numeroMasRepetido = numeros[i].textContent;
+            numeroMasRepetido = numeros[i].value;
             cantidadRepetido = repetido;
         }
 
@@ -49,8 +61,77 @@ function obtenerNumeroMasRepetido(numeros) {
     return numeroMasRepetido;
 }
 
-let numeros = document.querySelectorAll('.lista');
-document.querySelector('#promedio').textContent = calcularPromedio(numeros);
-document.querySelector('#numero-pequeño').textContent = obtenerNumeroPequeño(numeros);
-document.querySelector('#numero-grande').textContent = obtenerNumeroGrande(numeros);
-document.querySelector('#numero-frecuente').textContent = obtenerNumeroMasRepetido(numeros);
+function crearBoton(texto, id){
+    const botonCalcular = document.createElement('button');
+    botonCalcular.setAttribute('type', 'button');
+    botonCalcular.setAttribute('id', id);
+    const textoBoton = document.createTextNode(texto);
+    botonCalcular.appendChild(textoBoton);
+    return botonCalcular;
+}
+
+function crearNodoLista(){
+    const elementoLista = document.createElement('li');
+    const elementoInput = document.createElement('input');
+    elementoInput.setAttribute('type', 'number');
+    elementoInput.setAttribute('class', 'lista');
+    elementoLista.appendChild(elementoInput);
+    return elementoLista;
+}
+function crearLista(){
+    const elementoListaOrdenada = document.createElement('ol');
+    const labelLista = document.createElement('label');
+    const textoLabel = document.createTextNode('Ingrese los números');
+    labelLista.appendChild(textoLabel);
+    elementoListaOrdenada.appendChild(labelLista);
+    for (let i = 0; i < Number(document.querySelector('#cantidad-numeros-ingresar').value); i++) {
+        elementoListaOrdenada.appendChild(crearNodoLista());
+    }
+    return elementoListaOrdenada;
+}
+
+const $botonContinuar = document.querySelector('#continuar');
+$botonContinuar.onclick = function () {
+    const $body = document.querySelector('body');
+    $body.appendChild(crearLista());
+    $body.appendChild(crearBoton('Calcular el promedio', 'calcular-promedio'));
+    $body.appendChild(crearBoton('Obtener el número más pequeño', 'obtener-numero-pequenio'));
+    $body.appendChild(crearBoton('Obtener el número más grande', 'obtener-numero-grande'));
+    $body.appendChild(crearBoton('Obtener el número que más se repite', 'obtener-numero-frecuente'));
+    const elementoP = document.createElement('p');
+    elementoP.setAttribute('id', 'texto-resultado');
+    const $botonCalcularPromedio = document.querySelector('#calcular-promedio');
+    $botonCalcularPromedio.onclick = function() {
+        crearRespuesta('El promedio es ... ', 'promedio');
+        const numeros = document.querySelectorAll('.lista');
+        const promedio = calcularPromedio(numeros);
+        document.querySelector('#promedio').textContent = promedio;
+        return false;
+    }
+    const $botonObtenerNumeroPequenio = document.querySelector('#obtener-numero-pequenio');
+    $botonObtenerNumeroPequenio.onclick = function() {
+        crearRespuesta('El número más pequeño es ... ', 'numero-pequenio');
+        const numeros = document.querySelectorAll('.lista');
+        const numeroPequenio = obtenerNumeroPequenio(numeros);
+        document.querySelector('#numero-pequenio').textContent = numeroPequenio;
+        return false;
+    }
+    const $botonObtenerNumeroGrande = document.querySelector('#obtener-numero-grande');
+    $botonObtenerNumeroGrande.onclick = function() {
+        crearRespuesta('El número más grande es ... ', 'numero-grande');
+        const numeros = document.querySelectorAll('.lista');
+        const numeroGrande = obtenerNumeroGrande(numeros);
+        document.querySelector('#numero-grande').textContent = numeroGrande;
+        return false;
+    }
+    const $botonObtenerNumeroFrecuente = document.querySelector('#obtener-numero-frecuente');
+    $botonObtenerNumeroFrecuente.onclick = function() {
+        crearRespuesta('El número más frecuente es ... ', 'numero-frecuente');
+        const numeros = document.querySelectorAll('.lista');
+        const numeroFrecuente = obtenerNumeroMasRepetido(numeros);
+        document.querySelector('#numero-frecuente').textContent = numeroFrecuente;
+        return false;
+    }
+    $body.appendChild(elementoP);
+    return false;
+}
