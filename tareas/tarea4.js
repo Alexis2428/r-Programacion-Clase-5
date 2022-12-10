04071c8dd647fc611885e723ae9798a64e63e8e1
Alexis Landa <114886294+Alexis2428@users.noreply.github.com>
2022-12-10 13:18:54 -0300

    Mejora el código
    Mejora del código en base a lo aprendido hasta la clase 8 y agrega pequeños arreglos

--- tareas/tarea4.js
+++ tareas/tarea4.js
@@ -5,92 +5,6 @@
 // 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
 // 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
 
-function validarCantNumeros(cantNumeros) {
-    if ('' === cantNumeros) {
-        return 'El campo cantidad-numeros no puede estar vacio';
-    }
-
-    if (!/^[0-9]+$/.test(cantNumeros)) {
-        return 'El campo cantidad-numeros solo acepta números enteros';
-    }
-
-    return '';
-}
-function validarNumero(numero) {
-    if ('' === numero) {
-        return 'El campo numero no puede estar vacio';
-    }
-
-    return '';
-}
-const $botonContinuar = document.querySelector('#continuar');
-$botonContinuar.onclick = function (event) {
-    event.preventDefault();
-
-    borrarListaAnterior();
-    crearLista();
-}
-
-const $botonObtenerPromedio = document.querySelector('#obtener-promedio');
-$botonObtenerPromedio.onclick = function () {
-    const numeros = obtenerNumeros();
-
-    if ('' === validarNumeros(numeros)) {
-        obtenerRespuesta('promedio', obtenerPromedio(numeros).toFixed(2));
-        mostrarRespuesta('promedio');
-    } else {
-        alert(validarNumeros(numeros));
-    }
-}
-
-const $botonObtenerNumeroPequenio = document.querySelector('#obtener-numero-pequenio');
-$botonObtenerNumeroPequenio.onclick = function () {
-    const numeros = obtenerNumeros();
-
-    if ('' === validarNumeros(numeros)) {
-        obtenerRespuesta('pequenio', obtenerNumeroMenor(numeros));
-        mostrarRespuesta('pequenio');
-    } else {
-        alert(validarNumeros(numeros));
-    }
-}
-
-const $botonObtenerNumeroGrande = document.querySelector('#obtener-numero-grande');
-$botonObtenerNumeroGrande.onclick = function () {
-    const numeros = obtenerNumeros();
-
-    if ('' === validarNumeros(numeros)) {
-        obtenerRespuesta('grande', obtenerNumeroMayor(numeros));
-        mostrarRespuesta('grande');
-    } else {
-        alert(validarNumeros(numeros));
-    }
-}
-
-const $botonObtenerNumeroFrecuente = document.querySelector('#obtener-numero-frecuente');
-$botonObtenerNumeroFrecuente.onclick = function () {
-    const numeros = obtenerNumeros();
-
-    if ('' === validarNumeros(numeros)) {
-        if (1 < numeros.length) {
-            obtenerRespuesta('frecuente', obtenerNumeroFrecuente(numeros));
-            mostrarRespuesta('frecuente');
-        } else {
-            alert('Debe tener minimo 2 números para obtener el más frecuente');
-        }
-    } else {
-        alert(validarNumeros(numeros));
-    }
-}
-
-const $botonReiniciar = document.querySelector('#reiniciar');
-$botonReiniciar.onclick = function () {
-    ocultarRespuestas();
-    ocultarBotonesCalculos();
-    borrarListaAnterior();
-    ocultarBotonReiniciar();
-}
-
 function borrarListaAnterior() {
     const $numeros = document.querySelectorAll('.numero');
     for (let i = 0; i < $numeros.length; i++) {
@@ -98,16 +12,7 @@ function borrarListaAnterior() {
     }
 }
 
-function crearLista() {
-    const cantidadNumeros = Number(document.querySelector('#cantidad-numeros').value);
-    if (0 < cantidadNumeros) {
-        mostrarBotonesCalculos();
-        mostrarBotonReiniciar();
-    } else {
-        ocultarRespuestas();
-        ocultarBotonesCalculos();
-    }
-
+function crearLista(cantidadNumeros) {
     for (let i = 0; i < cantidadNumeros; i++) {
         crearNumero();
     }
@@ -129,13 +34,10 @@ function crearNumero() {
     $listaNumeros.appendChild($numero);
 }
 
-function obtenerNumeros() {
+function obtenerNumeros($numeros) {
     const numeros = [];
-    const $numeros = document.querySelectorAll('.numero input');
     for (let i = 0; i < $numeros.length; i++) {
-        if ('' !== $numeros[i].value) {
-            numeros.push(Number($numeros[i].value));
-        }
+        numeros.push(Number($numeros[i].value));
     }
     return numeros;
 }
@@ -144,10 +46,250 @@ function obtenerRespuesta(tipo, valor) {
     document.querySelector(`#numero-${tipo}`).textContent = valor;
 }
 
-function validarNumeros(numeros) {
-    if (0 === numeros.length) {
-        return 'Debe ingresar números en la lista antes de pulsar un botón';
+function validarCantidadNumeros($cantidadNumeros) {
+    const error = validarCantNumeros($cantidadNumeros.value);
+
+    const esValido = manejarError(error, $cantidadNumeros);
+
+    return esValido;
+}
+
+function validarCantNumeros(cantNumeros) {
+    if ('' === cantNumeros) {
+        return 'El campo cantidad-numeros no puede estar vacio';
+    }
+
+    if (!/^[0-9]+$/.test(cantNumeros)) {
+        return 'El campo cantidad-numeros solo acepta números enteros';
     }
 
     return '';
+}
+
+function manejarError(error, $cantidadNumeros) {
+    let noHayError = true;
+
+    if (error) {
+        noHayError = false;
+        $cantidadNumeros.className = 'error';
+
+        if (!comprobarExisteError(error)) {
+            crearError(error);
+        }
+
+    } else {
+        $cantidadNumeros.className = '';
+    }
+
+    const $errores = document.querySelectorAll('#errores li');
+
+    for (let i = 0; i < $errores.length; i++) {
+        if (error != $errores[i].innerText) {
+            $errores[i].remove();
+        }
+    }
+
+    return noHayError;
+}
+
+function comprobarExisteError(error) {
+    const $errores = document.querySelectorAll('#errores li');
+
+    for (let i = 0; i < $errores.length; i++) {
+        if (error === $errores[i].textContent) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function crearError(error) {
+    const $error = document.createElement('li');
+    $error.innerText = error;
+
+    document.querySelector('#errores').appendChild($error);
+}
+
+function validarNumeros($listaNumeros) {
+    const errores = {};
+    for (let i = 0; i < $listaNumeros.length; i++) {
+        errores[i] = validarNumero($listaNumeros[i].value);
+    }
+
+    const sonValidos = 0 === manejarErrores(errores, $listaNumeros)
+
+    return sonValidos;
+}
+
+function validarNumero(numero) {
+    if ('' === numero) {
+        return 'El campo numero no puede estar vacio';
+    }
+
+    return '';
+}
+
+function manejarErrores(errores, $numeros) {
+    let cantidadErrores = 0;
+
+    Object.keys(errores).forEach(function (key) {
+        const error = errores[key];
+
+        if (error) {
+            cantidadErrores++;
+            $numeros[key].className = 'error';
+
+            if (!comprobarExisteError(error)) {
+                crearError(error);
+            }
+
+        } else {
+            $numeros[key].className = '';
+        }
+    })
+
+    borrarErroresCorregidos(errores);
+
+    return cantidadErrores;
+}
+
+function borrarErroresCorregidos(errores) {
+    const $errores = document.querySelectorAll('#errores li');
+    const valorErrores = Object.values(errores);
+
+    for (let i = 0; i < $errores.length; i++) {
+        let existeEnErrores = false;
+
+        for (let j = 0; j < valorErrores.length; j++) {
+            if ($errores[i].innerText === valorErrores[j]) {
+                existeEnErrores = true;
+                break;
+            }
+        }
+
+        if (!existeEnErrores) {
+            $errores[i].remove();
+        }
+    }
+}
+
+function borrarErroresAnteriores() {
+    const $errores = document.querySelectorAll('#errores li');
+
+    for (let i = 0; i < $errores.length; i++) {
+        $errores[i].remove();
+    }
+}
+
+const $botonContinuar = document.querySelector('#continuar');
+$botonContinuar.onclick = function (event) {
+    event.preventDefault();
+
+    borrarListaAnterior();
+    borrarErroresAnteriores();
+
+    const $cantidadNumeros = document.querySelector('#cantidad-numeros');
+    if (validarCantidadNumeros($cantidadNumeros)) {
+        crearLista(Number($cantidadNumeros.value));
+
+        mostrarBotonesCalculos();
+        mostrarBotonReiniciar();
+
+    } else {
+        ocultarRespuestas();
+        ocultarBotonesCalculos();
+    }
+}
+
+const $botonObtenerPromedio = document.querySelector('#obtener-promedio');
+$botonObtenerPromedio.onclick = function () {
+    const $listaNumeros = document.querySelectorAll('.numero input');
+
+    const sonValidos = validarNumeros($listaNumeros);
+
+    if (sonValidos) {
+        const numeros = obtenerNumeros($listaNumeros);
+
+        obtenerRespuesta('promedio', obtenerPromedio(numeros).toFixed(2));
+        mostrarRespuesta('promedio');
+
+    } else {
+        ocultarRespuestas();
+    }
+}
+
+const $botonObtenerNumeroPequenio = document.querySelector('#obtener-numero-pequenio');
+$botonObtenerNumeroPequenio.onclick = function () {
+    const $listaNumeros = document.querySelectorAll('.numero input');
+
+    const sonValidos = validarNumeros($listaNumeros);
+
+    if (sonValidos) {
+        const numeros = obtenerNumeros($listaNumeros);
+
+        obtenerRespuesta('pequenio', obtenerNumeroMenor(numeros));
+        mostrarRespuesta('pequenio');
+
+    } else {
+        ocultarRespuestas();
+    }
+}
+
+const $botonObtenerNumeroGrande = document.querySelector('#obtener-numero-grande');
+$botonObtenerNumeroGrande.onclick = function () {
+    const $listaNumeros = document.querySelectorAll('.numero input');
+
+    const sonValidos = validarNumeros($listaNumeros);
+
+    if (sonValidos) {
+        const numeros = obtenerNumeros($listaNumeros);
+
+        obtenerRespuesta('grande', obtenerNumeroMayor(numeros));
+        mostrarRespuesta('grande');
+
+    } else {
+        ocultarRespuestas();
+    }
+}
+
+const $botonObtenerNumeroFrecuente = document.querySelector('#obtener-numero-frecuente');
+$botonObtenerNumeroFrecuente.onclick = function () {
+    const $listaNumeros = document.querySelectorAll('.numero input');
+
+    const sonValidos = validarNumeros($listaNumeros);
+
+    if (sonValidos) {
+        const error = 'Debe tener minimo 2 números para obtener el más frecuente';
+
+        if (1 < $listaNumeros.length) {
+            const $errores = document.querySelectorAll('#errores li');
+
+            for (let i = 0; i < $errores.length; i++) {
+                if (error === $errores[i].textContent) {
+                    $errores[i].remove();
+                }
+            }
+
+            const numeros = obtenerNumeros($listaNumeros);
+
+            obtenerRespuesta('frecuente', obtenerNumeroFrecuente(numeros));
+            mostrarRespuesta('frecuente');
+
+        } else {
+            crearError(error);
+            $listaNumeros[0].className = 'error';
+        }
+    } else {
+        ocultarRespuestas();
+    }
+}
+
+const $botonReiniciar = document.querySelector('#reiniciar');
+$botonReiniciar.onclick = function () {
+    ocultarRespuestas();
+    ocultarBotonesCalculos();
+    borrarListaAnterior();
+    borrarErroresAnteriores();
+    ocultarBotonReiniciar();
 }
